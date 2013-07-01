c47c4812aac32fc87b49bada220c07f8f569f23c
gvaduha <gvaduha@gmail.com>
2013-07-01 13:16:14 +0400

    Book formatter rewriten

--- PythonTests/formatbook.py
+++ PythonTests/formatbook.py
@@ -1,13 +1,25 @@
-with open(r'c:\aff.txt') as f:
-	with open(r'c:\yyy', mode='w') as of:
-		buff = ''
-		for line in f:
-			if line.isspace():
-				of.write(buff + '\n' + line)
-				buff = ''
-			if line[0] == r'"':
-				if len(buff)>0: of.write(buff + '\n')
-				buff = line.rstrip('\n')
-			else:
-				buff += line.rstrip('\n')
-		of.write(buff)
+import sys
+
+def format(filename):
+        def printbuff(buff):
+                if len(buff) > 0: print(buff)
+        with open(filename) as f:
+                buff = ''
+                for line in f:
+                        if line.isspace():
+                                printbuff(buff)
+                                print()
+                                buff = ''
+                                continue
+                        elif line[0].isspace() \
+                             or line[0] == r'"':  #direct speech?
+                                printbuff(buff)
+                                buff = ''
+                        buff += line.rstrip('\r\n') + ' '
+                print(buff)
+
+if __name__ == "__main__":
+        if len(sys.argv) < 2:
+                print("Error: input file name must be given")
+        else:
+                format(sys.argv[1])
