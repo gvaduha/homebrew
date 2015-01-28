f737a48d927b9a8b1784a1e5b02354f5271b140e
gvaduha <gvaduha@gmail.com>
2015-01-28 13:17:40 +0300

    Create source_code_helpers.sh

---
+++ Scripts/NixScripts/source_code_helpers.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Linecount by dir
+find . \( -name "*.cpp" -or -name "*.h*" \) -exec wc -l {} +
+# Linecount total
+find . -type f \( -name "*.cpp" -or -name "*.h*" \) -exec cat {} + | wc -l
+
+# Find all types of tags on comments i.e. //TODO: //HACK: //EXT: etc skipping namespaces like ATL::
+find . \( -name "*.cpp" -or -name "*.h*" \) -exec sed 's/.*\/\/[ \\t]*\([A-Z]\{2,10\}:\)[^:].\+$/\1/;tx;d;:x' {} + | sort | uniq
