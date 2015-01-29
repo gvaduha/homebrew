d82aeaea4c5299db8f270a3f94194e0f25022a86
gvaduha <gvaduha@gmail.com>
2015-01-29 08:57:46 +0300

    Update source_code_helpers.sh

--- Scripts/NixScripts/source_code_helpers.sh
+++ Scripts/NixScripts/source_code_helpers.sh
@@ -7,3 +7,6 @@ find . -type f \( -name "*.cpp" -or -name "*.h*" \) -exec cat {} + | wc -l
 
 # Find all types of tags on comments i.e. //TODO: //HACK: //EXT: etc skipping namespaces like ATL::
 find . \( -name "*.cpp" -or -name "*.h*" \) -exec sed 's/.*\/\/[ \\t]*\([A-Z]\{2,10\}:\)[^:].\+$/\1/;tx;d;:x' {} + | sort | uniq
+
+# Sync remote
+rsync -arv --exclude .svn --exclude .git ~/sources gvaduha@buildline:~/robot/queue
