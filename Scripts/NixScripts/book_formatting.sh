7368008876a49f96ce8d7f0883fd290db77d2ea0
gvaduha <gvaduha@gmail.com>
2016-02-25 12:22:46 +0300

    Update book_formatting.sh

--- Scripts/NixScripts/book_formatting.sh
+++ Scripts/NixScripts/book_formatting.sh
@@ -5,9 +5,12 @@ fi
 BOOKID=$1
 BOOKROOT="http://mreadz.com/new/index.php?id=$BOOKID"
 LASTPAGE=$2
-echo starting $BOOKROOT for $LASTPAGE pages
+BOOKNAME="_book.html"
+echo starting $BOOKROOT for $LASTPAGE pages in $BOOKNAME
+echo '<!DOCTYPE html><html><head><meta charset="utf-8"></head><body>' > $BOOKNAME
 for i in $(seq 1 1 $LASTPAGE)
 do 
 	wget $BOOKROOT\&pages=$i
 	egrep 'h3>|h4>|p>' index.php@id\=$BOOKID\&pages\=$i >> _book.html
 done
+echo '</body></html>' >> $BOOKNAME
