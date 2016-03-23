4691257b01c0f24ff8f39a4a2259775e3a1be881
gvaduha <gvaduha@gmail.com>
2016-03-23 13:33:46 +0300

    Update text_utils.sh

--- Scripts/NixScripts/text_utils.sh
+++ Scripts/NixScripts/text_utils.sh
@@ -5,3 +5,7 @@
 # count duplicate words
 IFS=$',.;:-\r\n\t '; words=($(<"$1"))
 echo "${words[@]}" | tr ' ' '\n' | LC_ALL='C' sort | LC_ALL='C' uniq -c -d
+
+#parse log excluding columns
+awk -F, '{printf $1FS$7; for(i=11;i<=NF;++i) {printf FS$i;} printf "\n"}'
+
