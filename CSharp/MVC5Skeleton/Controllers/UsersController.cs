56b997012942507703b8f5d65482d4460618a2c8
gvaduha <gvaduha@gmail.com>
2016-04-19 14:36:06 +0300

    MVC5Skeleton: added MVC5 Controllers and Views for VirtualRooms and Users

---
+++ CSharp/MVC5Skeleton/Controllers/UsersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Skeleton;
+
+namespace MVC5Skeleton.Controllers
+{
+    public class UsersController : Controller
+    {
+        private EntityDataModelContainer db = new EntityDataModelContainer();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var userSet = db.UserSet.Include(u => u.VirtualRoom);
+            return View(userSet.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.UserSet.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            ViewBag.VirtualRoomId = new SelectList(db.VirtualRoomSet, "Id", "Name");
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,VirtualRoomId")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserSet.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.VirtualRoomId = new SelectList(db.VirtualRoomSet, "Id", "Name", user.VirtualRoomId);
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.UserSet.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.VirtualRoomId = new SelectList(db.VirtualRoomSet, "Id", "Name", user.VirtualRoomId);
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,VirtualRoomId")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.VirtualRoomId = new SelectList(db.VirtualRoomSet, "Id", "Name", user.VirtualRoomId);
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.UserSet.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.UserSet.Find(id);
+            db.UserSet.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
