f14625b7e6e640b1d8548e04e7dcab2d2aac446b
gvaduha <gvaduha@gmail.com>
2016-04-19 13:20:38 +0300

    MVC5Skeleton created

---
+++ CSharp/MVC5Skeleton/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5Skeleton.Startup))]
+namespace MVC5Skeleton
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
