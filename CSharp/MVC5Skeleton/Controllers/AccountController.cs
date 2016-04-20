18461f1abdf7fc8ebc39e1b3e5a137a4c4298283
gvaduha <gvaduha@gmail.com>
2016-04-20 11:20:34 +0300

    MVC5Skeleton: added security to DomainData views and links to it to the main page

--- CSharp/MVC5Skeleton/Controllers/AccountController.cs
+++ CSharp/MVC5Skeleton/Controllers/AccountController.cs
@@ -18,6 +18,9 @@ namespace MVC5Skeleton.Controllers
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
+        private EntityDataModelContainer mainDb = new EntityDataModelContainer();
+
+
         public AccountController()
         {
         }
@@ -156,12 +159,18 @@ namespace MVC5Skeleton.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    //// Add new user to DomainDB
+                    //var domainUser = new MVC5Skeleton.User();
+                    //domainUser.Name = user.UserName;
+                    //mainDb.UserSet.Add(domainUser);
+                    //mainDb.SaveChanges();
 
                     return RedirectToAction("Index", "Home");
                 }
