4b834853b6c4b88712b1c3506b95ed0f265131ff
gvaduha <gvaduha@gmail.com>
2016-08-31 13:01:38 +0300

    Update create_rescue_disk.sh

--- Scripts/NixScripts/create_rescue_disk.sh
+++ Scripts/NixScripts/create_rescue_disk.sh
@@ -8,14 +8,14 @@ part=$device$partnum
 
 echo "Installing to $part"
 
-#mkfs.ext4 $part
+mkfs.ext4 $part
 
 mount $part /mnt
 
-#force in case of "do not proceed with blocklists"
-#sudo grub-install --force --root-directory=/mnt $part
+# force in case of "do not proceed with blocklists"
+sudo grub-install --force --root-directory=/mnt $part
 
-#write grub.cfg
+# write grub.cfg
 cat > /mnt/boot/grub/grub.cfg << EOF
 
 if loadfont /boot/grub/font.pf2 ; then
@@ -33,6 +33,16 @@ set menu_color_highlight=black/light-gray
 #set gfxpayload=keep
 #linux/casper/vmlinuz.efi  file=/cdrom/preseed/lubuntu.seed boot=casper quiet splash ---
 #initrd/casper/initrd.lz
+#}
+
+# need to find UUID of partition with blkid
+#menuentry "Windows 7 (loader) (on /dev/sda1)" --class windows --class os {
+#    insmod part_msdos
+#    insmod ntfs
+#    insmod ntldr
+#    set root='(hd0,msdos1)'
+#    search --no-floppy --fs-uuid --set=root 1EA0019AA0017A13
+#    ntldr ($root)/bootmgr
 #}
 
 menuentry "GParted Live" {
