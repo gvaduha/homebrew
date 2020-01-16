da20a7faf6171a787f8032e55005515799c473fd
gvaduha <gvaduha@gmail.com>
2020-01-16 11:10:41 +0700

    Update async-process-collection.cs

--- csharp/test-sketches/async-process-collection.cs
+++ csharp/test-sketches/async-process-collection.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace asynctest
@@ -10,15 +11,23 @@ namespace asynctest
         Random r = new Random(DateTime.Now.Millisecond);
         async Task<bool> S(int i)
         {
-            Console.WriteLine($"pre:  {i}");
+            Console.WriteLine($"pre:  {i} [thread:{Thread.CurrentThread.ManagedThreadId}]");
             await Task.Delay(r.Next(0, 10) * 500);
-            Console.WriteLine($"post: {i}");
+            Console.WriteLine($"post: {i} [thread:{Thread.CurrentThread.ManagedThreadId}]");
             return i%2 == 0;
         }
 
         public IReadOnlyCollection<int> Run(IEnumerable<int> l)
         {
             return l.Select(x => new { Item = x, Oper = S(x) }).Where(x => x.Oper.Result).Select(x=>x.Item).ToArray();
+        }
+
+        public IReadOnlyCollection<int> RunAsync(IEnumerable<int> l)
+        {
+            var tasks = l.Select(x => new { Item = x, Oper = S(x) });
+            Console.WriteLine("Tasks prepared");
+            Task.WhenAll(tasks.Select(x => x.Oper));
+            return tasks.Where(x => x.Oper.Result).Select(x => x.Item).ToArray();
         }
     }
 
@@ -28,9 +37,13 @@ namespace asynctest
         {
             var x = new X();
             Console.WriteLine("Run #1");
-            x.Run(Enumerable.Range(1, 10)).ToList().ForEach(x=>Console.WriteLine(x));
+            x.Run(Enumerable.Range(1, 10)).ToList().ForEach(x => Console.WriteLine(x));
             Console.WriteLine("Run #2");
             x.Run(Enumerable.Range(50, 10)).ToList().ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("RunAsync #1");
+            x.RunAsync(Enumerable.Range(1, 10)).ToList().ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("RunAsync #2");
+            x.RunAsync(Enumerable.Range(50, 10)).ToList().ForEach(x => Console.WriteLine(x));
         }
     }
 }
